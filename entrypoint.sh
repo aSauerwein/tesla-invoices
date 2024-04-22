0eee059e054ff48e135bf18fcbf8dba0cf741f46
Andreas Sauerwein-Schlosser <andreas.sauerwein@nts.eu>
2024-04-22 14:26:38 +0200

    just start once on container startup

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-print "Start Tesla-Invoices hourly cron job"
 printenv >> /etc/environment
+# trigger download once on startup
+/usr/local/bin/python3 /opt/tesla-invoices/download_v2.py daemon > /proc/1/fd/1 2>&1
 cron -f
